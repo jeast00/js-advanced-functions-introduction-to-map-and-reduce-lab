8e7c1990c17debc5c7dd2517de0c5abb6f2f1b80
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-03-20 14:11:57 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Your code here
+
+
+function mapToNegativize(num) {
+    let n = [];
+
+    for(let i = 0; i < num.length; i++) {
+        n.push(-1 * num[i]);
+    }
+    return n;
+}
+
+function mapToNoChange(num) {
+    let n = [];
+
+    for(let i = 0; i < num.length; i++) {
+        n.push(num[i]);
+    }
+    return n;
+}
+
+function mapToDouble(num) {
+    let n = [];
+
+    for(let i = 0; i < num.length; i++) {
+        n.push(2 * num[i]);
+    }
+    return n;
+}
+
+function mapToSquare(num) {
+    let n = [];
+
+    for(let i = 0; i < num.length; i++) {
+        n.push(num[i] * num[i]);
+    }
+    return n;
+}
+
+//---------------------------------------------------------
+
+
+function reduceToTotal(num, startPoint = 0) {
+    let total = startPoint;
+
+    for(let i = 0; i < num.length; i++) {
+        total = total + num[i];
+    }
+    return total;
+}
+
+function reduceToAllTrue(b) {
+
+    for(let i = 0; i < b.length; i++) {
+        if(!b[i]) return false;
+    }
+    return true;
+}
+
+function reduceToAnyTrue(b) {
+    
+    for(let i = 0; i < b.length; i++) {
+        if(b[i]) return true;
+    }
+    return false;
+}
